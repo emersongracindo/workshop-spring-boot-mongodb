3e4e6ce8de7d84ea71d9b8e62f22115631a0752b
Emerson Gracindo <emersongracindo@gmail.com>
2018-09-14 10:50:30 -0300

    Obtendo um usuario por id

--- src/main/java/com/emersongracindo/workshopmongo/services/UserService.java
+++ src/main/java/com/emersongracindo/workshopmongo/services/UserService.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Service;
 
 import com.emersongracindo.workshopmongo.domain.User;
 import com.emersongracindo.workshopmongo.repository.UserRepository;
+import com.emersongracindo.workshopmongo.services.exception.ObjectNotFoundException;
 
 @Service
 public class UserService {
@@ -16,6 +17,14 @@ public class UserService {
 	
 	public List<User> findAll(){
 		return repo.findAll();
+	}
+	
+	public User findById(String id) {
+		User user = findById(id); // aqui o nelio usou findOne
+		if(user == null) {
+			throw new ObjectNotFoundException("Objeto não encontrado.");
+		}
+		return user;
 	}
 
 }
