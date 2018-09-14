d8321dfb16530ca75250e123a5a99a9c9b06db82
Emerson Gracindo <emersongracindo@gmail.com>
2018-09-14 11:35:52 -0300

    Inserção de usuário com Post

--- src/main/java/com/emersongracindo/workshopmongo/services/UserService.java
+++ src/main/java/com/emersongracindo/workshopmongo/services/UserService.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.emersongracindo.workshopmongo.domain.User;
+import com.emersongracindo.workshopmongo.dto.UserDTO;
 import com.emersongracindo.workshopmongo.repository.UserRepository;
 import com.emersongracindo.workshopmongo.services.exception.ObjectNotFoundException;
 
@@ -26,5 +27,12 @@ public class UserService {
 		}
 		return user;
 	}
-
+	
+	public User insert(User obj) {
+		return repo.insert(obj);
+	}
+	
+	public User fromDTO(UserDTO objDto) {
+		return new User(objDto.getId(), objDto.getName(), objDto.getEmail());
+	}
 }
